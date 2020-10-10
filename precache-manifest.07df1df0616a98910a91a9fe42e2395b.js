1b7efe7bc50101a68a1408bb013222b8547a3cce
Slavik Ohorodnik <teatrgsc@gmail.com>
2020-10-10 14:42:04 +0300

    Updates

---
+++ precache-manifest.07df1df0616a98910a91a9fe42e2395b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0cd8c454e9eca7283080aa85952365f",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "7d9f7954590664b150b1",
+    "url": "/goit-react-hw-03-image-finder/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "406407268ed3177dfc09",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.1ec7c5c9.chunk.css"
+  },
+  {
+    "revision": "7d9f7954590664b150b1",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.60a87786.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.60a87786.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "406407268ed3177dfc09",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.29cfea61.chunk.js"
+  },
+  {
+    "revision": "406fb7c160348e729de8",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.72ef0ae4.js"
+  }
+]);
